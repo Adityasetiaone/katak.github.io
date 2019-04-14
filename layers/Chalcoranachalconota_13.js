ded24cc220db74578ec1817f15eef2900b06abeb
Adityasetiaone <adityasetiawan00@gmail.com>
2019-04-14 22:04:33 +0800

    projectfiles remove from server

--- layers/Chalcoranachalconota_13.js
+++
@@ -1,8 +0,0 @@
-var json_Chalcoranachalconota_13 = {
-"type": "FeatureCollection",
-"name": "Chalcoranachalconota_13",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "Habitat": "Terestrial", "Status": "Least Concern", "Substrat": "Daun, Ranting, Tanah", "Famili": "Ranidae" }, "geometry": { "type": "Point", "coordinates": [ 117.193473820391944, -0.333295944612847 ] } }
-]
-}
--- layers/Microhylaberdmorei_10.js
+++
@@ -1,8 +0,0 @@
-var json_Microhylaberdmorei_10 = {
-"type": "FeatureCollection",
-"name": "Microhylaberdmorei_10",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "Habitat": "Terestrial", "Status": "Least Concern", "Substrat": "Tanah,Serasah", "Famili": "Megophryidae" }, "geometry": { "type": "Point", "coordinates": [ 117.200517040652286, -0.33464072682483 ] } }
-]
-}
